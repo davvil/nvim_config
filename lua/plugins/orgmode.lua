51023a6f832c2fc7c5eb08c97db198fdeeb32d86
David Vilar <david.vilar@gmail.com>
2023-12-14 12:51:33 +0100

    Changed icon of undone tasks in neorg

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -7,7 +7,17 @@ return {
       require("neorg").setup {
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.concealer"] = { -- Adds pretty icons to your documents
+            config = {
+              icons = {
+                todo = {
+                  undone = {
+                    icon = " ",
+                  }
+                }
+              }
+            }
+          },
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
               workspaces = {
