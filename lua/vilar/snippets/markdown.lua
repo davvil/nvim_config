093089edc9d8b5caaec8254c502d7cbb5574aa5c
David Vilar <david.vilar@gmail.com>
2024-09-19 13:21:00 +0200

    Cleaning up snippets

--- lua/vilar/snippets/markdown.lua
+++ lua/vilar/snippets/markdown.lua
@@ -4,17 +4,6 @@ local n = require('vilar.snippets.nodes')
 local fmt = require('luasnip.extras.fmt').fmt
 
 ls.add_snippets("markdown", {
-  s("@xid", fmt("[xid/{xid}](http://xid/{xid_url})", {
-    xid = n.i(1),
-    xid_url = n.f(function(xid) return xid[1][1] end, {1}),
-  })),
-
-  s("@cl", fmt("[cl/{cl}](http://cl/{cl_url})", {
-    cl = n.i(1),
-    cl_url = n.f(function(cl) return cl[1][1] end, {1}),
-  })),
-
-
   s("@url", fmt("[{link}]({url})", {
     link = n.i(1),
     url = n.i(2),
